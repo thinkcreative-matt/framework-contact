e3d290dcd60f8c653d9f06c9f393480c2611c364
thinkcreative-matt <56924270+thinkcreative-matt@users.noreply.github.com>
2019-11-06 13:17:50 +0000

    module from contact

--- src/database/migrations/2019_11_05_130418_modules_table_from_contact.php
+++ src/database/migrations/2019_11_05_130418_modules_table_from_contact.php
@@ -4,9 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use Thinkcreative\Blog\TCModule;
+use Thinkcreative\Contact\TCModule;
 
-class ModulesTableFromBlog extends Migration
+class ModulesTableFromContact extends Migration
 {
     /**
      * Run the migrations.
